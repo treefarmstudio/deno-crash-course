a757700183764d1ec9a83832a548f37f5a39fec5
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-12-15 13:28:47 -0700

    part 4 files

---
+++ api/main.js
@@ -0,0 +1,76 @@
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+// import { v4 } from "https://deno.land/std@0.168.0/uuid/mod.ts";
+
+const app = new Application();
+
+const router = new Router();
+
+const people = [
+  {
+    id: 1,
+    slug: 'luke-skywalker',
+    name: 'Luke Skywalker',
+    homeWorld: 'Tatooine',
+  },
+  {
+    id: 2,
+    slug: 'leia-organa',
+    name: 'Leia Organa',
+    homeWorld: 'Alderaan',
+  },
+  {
+    id: 3,
+    slug: 'han-solo',
+    name: 'Han Solo',
+    homeWorld: 'Corellia',
+  },
+  {
+    id: 4,
+    slug: 'darth-vader',
+    name: 'Darth Vader',
+    homeWorld: 'Tatooine',
+  }
+]
+
+router
+.get('/', (ctx) => {
+  ctx.response.body = 'Hello from our API! 🦕'
+})
+.get('/people', (ctx) => {
+  ctx.response.body = people;
+})
+.get('/people/:slug', (ctx) => {
+  const {slug} = ctx.params;
+  const person = people.find((person) => person.slug === slug);
+  if(person) {
+    ctx.response.body = person;
+  } else {
+    ctx.response.body = 'That person was not found 😭'
+  }
+})
+.post('/people', async (ctx) => {
+  const { id, slug, name, homeWorld } = await ctx.request.body('json').value
+  const person = {
+    id,
+    slug,
+    name,
+    homeWorld
+  }
+  if(person) {
+    people.push(person)
+    ctx.response.body = people
+  } else {
+    ctx.response.body = "Person not added 😭"
+  }
+})
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener('listen', () => {
+  console.log('App is running on http://localhost:8000');
+})
+
+app.use();
+
+await app.listen({port: 8000})
--- data.js
+++
@@ -1,7 +0,0 @@
-const data = await fetch('https://swapi.dev/api/people')
-
-const results = await data.json()
-
-const encoder = new TextEncoder()
-
-await Deno.writeFile('./data.json', encoder.encode(JSON.stringify(results)))
